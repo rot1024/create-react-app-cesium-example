7882419d0e5f07bf9f5156240d3c5015f9625590
rot1024 <aayhrot@gmail.com>
2019-12-24 15:33:18 +0900

    disable hot-loader

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
-import { hot } from 'react-hot-loader/root';
-import { Viewer } from 'resium';
+// import { hot } from 'react-hot-loader/root';
+import { Viewer, Entity } from 'resium';
+import { Cartesian3 } from 'cesium';
 
 function App() {
   return (
-    <Viewer full />
+    <Viewer full>
+      <Entity position={Cartesian3.fromDegrees(0, 0, 0)} point={{ pixelSize: 20 }} />
+    </Viewer>
   );
 }
 
-export default hot(App);
+export default App;
+// export default hot(App);
