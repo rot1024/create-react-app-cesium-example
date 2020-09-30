9579192810981c11b7540ba2dcab72f8a9eecd7e
rot1024 <aayhrot@gmail.com>
2020-09-30 12:27:44 +0900

    update

--- src/App.js
+++ src/App.js
@@ -1,20 +1,19 @@
 import React from 'react';
 import { hot } from 'react-hot-loader/root';
-import { Viewer, Entity, BoxGraphics } from 'resium';
-import { Cartesian3, Color } from 'cesium';
+import { Viewer, Entity, PointGraphics, EntityDescription } from 'resium';
+import { Cartesian3 } from 'cesium';
 
 function App() {
   return (
     <Viewer full>
       <Entity
         name="BoxGraphics"
-        description="BoxGraphics!!"
         position={Cartesian3.fromDegrees(0, 0, 0)}
       >
-        <BoxGraphics
-          material={Color.RED}
-          dimensions={new Cartesian3(400000.0, 300000.0, 500000.0)}
-        />
+        <PointGraphics pixelSize={20} />
+        <EntityDescription>
+          <h1>Hello</h1>
+        </EntityDescription>
       </Entity>
     </Viewer>
   );
